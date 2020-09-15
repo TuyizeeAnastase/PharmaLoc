d94337e94ec0977987f23a1efd13841b804ce0ba
TuyizeeAnastase <42033331+TuyizeeAnastase@users.noreply.github.com>
2020-09-15 14:32:56 +0200

    adding all APIS for Paharmacies and Insurance

---
+++ src/controllers/pharmaController.js
@@ -0,0 +1,20 @@
+
+exports.getPharmacies=(req,res)=>{
+    res.send('get ALL')
+}
+
+exports.addPharmacy=(req,res)=>{
+    res.send('Add one')
+}
+
+exports.getPharmacy=(req,res)=>{
+    res.send('get one')
+}
+
+exports.editPharmacy=(req,res)=>{
+    res.send('editing')
+}
+
+exports.deletePharamacy=(req,res)=>{
+    res.send('deleting')
+}
